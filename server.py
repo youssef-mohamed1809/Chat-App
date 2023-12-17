96b13bf688591bf72d52704ba42c80726bfd25cc
Ziad Mahmoud <ziyadmahmoud63@gmail.com>
2023-12-17 15:07:56 +0200

    Created view online users after login

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ DISCONNECT = "!DISCONNECT"
 
 connected_clients = []
 
+def show_online_users():
+    online_users = [addr[0] for addr, authenticated in connected_clients.items() if authenticated]
+    print(f"Online Users: {online_users}")
+
 def handle_login():
     msg_length = conn.recv(HEADER).decode(FORMAT)
     if msg_length:
@@ -33,6 +37,7 @@ def handle_login():
         response = ""
         if login:
             response = "Success"
+            connected_clients[addr] = True
         else:
             response = msg
 
@@ -78,6 +83,8 @@ def handle_register():
 # Whenever a client connects
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
+    connected_clients[addr] = False
+
     connected = True
     while connected:
         # Get from the client the message length they want to send
@@ -86,13 +93,24 @@ def handle_client(conn, addr):
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
 
-            if msg == "Login":
-                handle_login()
-            elif msg == "Register":
-                handle_register()
-            elif msg == DISCONNECT:
-                connected = False
+            if not connected_clients[addr]:
+                # If the client is not authenticated, only allow login or register
+                if msg == "Login":
+                    handle_login(conn, addr)
+                elif msg == "Register":
+                    handle_register(conn, addr)
+            else:
+                # If the client is authenticated, allow additional commands
+                if msg == "OnlineUsers":
+                    show_online_users()
+                elif msg == DISCONNECT:
+                    connected = False
+    
     conn.close()
+    del connected_clients[addr]
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     print("STARTING SERVER")
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
